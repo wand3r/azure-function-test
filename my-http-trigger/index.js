9daeee6d9e4f9ebf91582c01c049806adead5ee3
Wojciech Karaś <wkaras89@gmail.com>
2018-11-13 15:41:00 +0100

    .

--- my-http-trigger/index.js
+++ my-http-trigger/index.js
@@ -3,7 +3,7 @@ module.exports = async function(context, req) {
   if (req.query.name || (req.body && req.body.name)) {
     context.res = {
       // status: 200, /* Defaults to 200 */
-      body: "Hello " + (req.query.name || req.body.name)
+      body: "Helloooo " + (req.query.name || req.body.name)
     };
   } else {
     context.res = {
